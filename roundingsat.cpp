505b4f9a0424b458d9de9fd7ead5d24aad38d593
Jan Elffers <elffersj@gmail.com>
2019-03-30 23:58:36 +0100

    update copyright

--- roundingsat.cpp
+++ roundingsat.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************
-RoundingSat -- Copyright (c) 2014-2018, Jan Elffers
+RoundingSat -- Copyright (c) 2014-2019, Jan Elffers
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
