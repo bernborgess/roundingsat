0241d35824af4f8a6439c0d39fc5707d5ea6ac17
elffersj <47174890+elffersj@users.noreply.github.com>
2019-08-28 00:11:32 +0200

    Update roundingsat.cc

--- roundingsat.cc
+++ roundingsat.cc
@@ -1,5 +1,6 @@
 /***********************************************************************
 Copyright (c) 2014-2019, Jan Elffers
+Copyright (c)      2019, Jo Devriendt
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
