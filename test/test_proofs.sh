24a9b67ae80487ef22ef22b5907957e1cffa9ebe
JoD <holkann@gmail.com>
2020-05-12 14:45:26 +0200

    simple unlocalized testing

--- test/test_proofs.sh
+++ test/test_proofs.sh
@@ -2,7 +2,6 @@
 
 cd ..
 make debug
-#g++ -o roundingsat_debug roundingsat.cc -g -O3
 cd test
 
 time=$1
@@ -16,67 +15,41 @@ echo "########## START ##########"
 echo "###########################"
 echo ""
 
-i="maxsat"
-echo "running $i"
-rm $i.proof
-rm $i.formula
-bzcat /home/jod/workspace/instances/maxsat/mse19-complete-weighted-benchmarks/planning/driverlog01c.wcsp.dir.wcnf.bz2 | ../roundingsat_debug --proof-log=$i > /dev/null
-echo "verifying $i"
-wc -l $i.proof
-veripb $i.formula $i.proof -d --arbitraryPrecision
-tested=`expr 1 + $tested`
-echo $tested
-errors=`expr $? + $errors`
-echo $errors
-echo ""
-
-i="cnf"
-echo "running $i"
-rm $i.proof
-rm $i.formula
-bzcat /home/jod/workspace/instances/dec/CNF/even_colouring/ec_rand4regsplit-v030-n1.cnf.bz2 | ../roundingsat_debug --proof-log=$i > /dev/null
-echo "verifying $i"
-wc -l $i.proof
-veripb $i.formula $i.proof -d --arbitraryPrecision
-tested=`expr 1 + $tested`
-echo $tested
-errors=`expr $? + $errors`
-echo $errors
-echo ""
-
-declare -a arr_dec=(
-"stein9"
-"stein15"
-"p0040"
-"pipex"
-"bm23"
-"p0291"
-"sentoy"
-"lp4l"
-"cracpb1"
-"air01"
-"air02"
-"air06"
+declare -a arr_default=(
+"maxsat"
+"cnf"
+"opb/dec"
 )
 
-echo "*** decision ***"
+echo "########## simple #########"
 echo ""
-for i in "${arr_dec[@]}"
-do
-    echo "running $i"
-    rm $i.proof
-    rm $i.formula
-    bzcat /home/jod/workspace/instances/dec/MIPLIB/miplib2/$i.0.u.opb.bz2 > $i.opb
-    timeout $time ../roundingsat_debug $i.opb --proof-log=$i > /dev/null
-    echo "verifying $i"
-    wc -l $i.proof
-    veripb $i.formula $i.proof -d --arbitraryPrecision
-    tested=`expr 1 + $tested`
-    echo $tested
-    errors=`expr $? + $errors`
-    echo $errors
-    echo ""
+for folder in "${arr_default[@]}"; do
+    echo $folder
+    for formula in instances/$folder/*; do
+        echo "running $formula"
+        logfile=${formula/instances/logs}
+        mkdir -p `dirname $logfile`
+        echo -n "" > $logfile.proof
+        echo -n "" > $logfile.formula
+        timeout $time ../roundingsat_debug $formula --proof-log=$logfile > /dev/null
+        echo "verifying $logfile"
+        wc -l $logfile.proof
+        veripb $logfile.formula $logfile.proof -d --arbitraryPrecision
+        tested=`expr 1 + $tested`
+        echo $tested
+        errors=`expr $? + $errors`
+        echo $errors
+        echo ""
+    done
 done
+
+declare -a arr_modes=(
+"linear"
+"core-guided"
+"lazy-core-guided"
+"hybrid"
+"lazy-hybrid"
+)
 
 declare -a arr_opt=(
 "enigma_0"
@@ -104,33 +77,27 @@ declare -a arr_opt=(
 "lseu_1120"
 "cracpb1_22199"
 )
-
-declare -a arr_modes=(
-"linear"
-"core-guided"
-"lazy-core-guided"
-"hybrid"
-"lazy-hybrid"
-)
-
+    
 for mode in "${arr_modes[@]}"; do
-    echo "*****  $mode  *****"
+    echo "########## $mode ##########"
     echo ""
     for j in "${arr_opt[@]}"; do
-        i="$(cut -d'_' -f1 <<<$j)"
+        formula="$(cut -d'_' -f1 <<<$j)"
+        formula="instances/opb/opt/$formula.opb"
         obj="$(cut -d'_' -f2 <<<$j)"
-        echo "running $i"
-        rm $i.proof
-        rm $i.formula
-        bzcat /home/jod/workspace/instances/opt/MIPLIB/miplib2/$i.opb.bz2 > $i.opb
-        output=`timeout $time ../roundingsat_debug $i.opb --proof-log=$i --opt-mode=$mode | awk '/^o/ {print $2}'`
+        echo "running $formula"
+        logfile=${formula/instances/logs}
+        mkdir -p `dirname $logfile`
+        echo -n "" > $logfile.proof
+        echo -n "" > $logfile.formula
+        output=`timeout $time ../roundingsat_debug $formula --proof-log=$logfile --opt-mode=$mode | awk '/^o/ {print $2}'`
         if [ "$output" != "" ] && [ "$output" != "$obj" ]; then
-            errors=`expr 100 + $errors`
+            errors=`expr 1000 + $errors`
             echo "wrong output: $output vs $obj"
         fi
-        echo "verifying $i"
-        wc -l $i.proof
-        veripb $i.formula $i.proof -d --arbitraryPrecision
+        echo "verifying $logfile"
+        wc -l $logfile.proof
+        veripb $logfile.formula $logfile.proof -d --arbitraryPrecision
         tested=`expr 1 + $tested`
         echo $tested
         errors=`expr $? + $errors`
@@ -140,3 +107,4 @@ for mode in "${arr_modes[@]}"; do
 done
 
 echo "tested: $tested"
+echo "errors: $errors"
